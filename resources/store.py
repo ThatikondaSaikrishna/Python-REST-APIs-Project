a818de8727a8a73a5ad0cc9917921fa9dfbb3f5b
ThatikondaSaikrishna <thatikondasaikrishna111@gmail.com>
2023-11-01 00:00:48 -0600

    First commit!

---
+++ resources/store.py
@@ -0,0 +1,49 @@
+import uuid
+from flask import request
+from flask.views import MethodView
+from flask_smorest import Blueprint, abort
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
+
+from db import db
+from models import StoreModel
+from schemas import StoresSchema
+
+blp = Blueprint("Stores", "stores", description="Operations on stores")
+
+
+@blp.route("/store/<string:store_id>")
+class Store(MethodView):
+    @blp.response(200, StoresSchema)
+    def get(self, store_id):
+        store = StoreModel.query.get_or_404(store_id)
+        return store
+
+    def delete(self, store_id):
+        store = StoreModel.query.get_or_404(store_id)
+        db.session.delete(store)
+        db.session.commit()
+        return {"message": "Store Deleted"}
+
+
+@blp.route("/store")
+class StoreList(MethodView):
+    @blp.response(200, StoresSchema(many=True))
+    def get(self):
+        return StoreModel.query.all()
+
+    @blp.arguments(StoresSchema)
+    @blp.response(200, StoresSchema)
+    def post(self, store_data):
+        store = StoreModel(**store_data)
+        try:
+            db.session.add(store)
+            db.session.commit()
+        except IntegrityError:
+            abort(
+                400,
+                message="A store with that name already exists.",
+            )
+        except SQLAlchemyError:
+            abort(500, message="An error occurred creating the store.")
+
+        return store
